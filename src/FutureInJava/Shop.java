5e0239f82fb926a7bdfbb0c6f15e996cadfd4d9a
Sheikh Sayed <sksayed@live.com>
2020-06-29 17:55:49 +0600

    End Of First Part

--- src/FutureInJava/Shop.java
+++ src/FutureInJava/Shop.java
@@ -5,42 +5,52 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 public class Shop {
+    private String shopName;
 
-    public double getPriceSync(String shopName) {
-        return calculatePrice(shopName) ;
+    public Shop(String shopName) {
+        this.shopName = shopName;
     }
 
-    public Future<Double> getPriceAsync ( String shopName) {
-        return calculatePriceAsync(shopName);
+    public double getPriceSync() {
+        return calculatePrice(this.shopName);
+    }
+
+    public Future<Double> getPriceAsync() {
+        return calculatePriceAsync(this.shopName);
         //return 0.0;
     }
 
     private Future<Double> calculatePriceAsync(String shopName) {
         CompletableFuture<Double> futurePrice = new CompletableFuture<>();
-        new Thread( ()->{
-            double price = calculatePrice(shopName);
-            futurePrice.complete(price);
+        new Thread(() -> {
+            try {
+                double price = calculatePrice(shopName);
+                futurePrice.complete(price);
+            } catch (Exception e) {
+                futurePrice.completeExceptionally(e);
+            }
+
         }).start();
 
-        return  futurePrice;
+        return futurePrice;
     }
 
-    private void delay () {
+    private void delay() {
         try {
             //here we are making the thread sleeping for
             //five seconds
             String threadName = Thread.currentThread().getName();
-            System.out.println("executing on "+threadName);
-            Thread.sleep(1*1000L);
+            System.out.println("executing on " + threadName);
+            Thread.sleep(1 * 1000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private double calculatePrice (String name){
+    private double calculatePrice(String name) {
         Random random = new Random();
         delay();
-        return random.nextDouble()*name.charAt(0)+name.charAt(1);
+        return random.nextDouble() * name.charAt(0) + name.charAt(1);
     }
 
 }
--- src/FutureInJava/TestingShopApi.java
+++ src/FutureInJava/TestingShopApi.java
@@ -5,14 +5,19 @@ import java.util.concurrent.Future;
 
 public class TestingShopApi {
     public static void main(String[] args) {
-        Shop s = new Shop();
+        doingSyncAndAsyncCalling();
+
+    }
+
+    private static void doingSyncAndAsyncCalling() {
+        Shop s = new Shop("sayed");
         long startOfSync = System.nanoTime();
-        double price = s.getPriceSync("sayed");
+        double price = s.getPriceSync();
         System.out.println("Price is "+price);
         long endTimeSync = System.nanoTime();
         System.out.println("time required is "+((endTimeSync-startOfSync)/1_000_000)+" milisecs");
         long startOfAync = System.nanoTime();
-        Future<Double> getPriceAsync = s.getPriceAsync("leona");
+        Future<Double> getPriceAsync = s.getPriceAsync();
         long invocationTime = ((System.nanoTime()- startOfAync)/1_000_000);
         System.out.println("Invocation returned after "+invocationTime+" milisecs");
         //doing something else
@@ -26,6 +31,5 @@ public class TestingShopApi {
 
         long retrivalTime  = ((System.nanoTime()- invocationTime)/1_000_000);
         System.out.println("Price returned after "+(retrivalTime/1000)+" secs");
-
     }
 }
