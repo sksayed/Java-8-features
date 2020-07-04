0dd6ab1e9d1e85e43bb0e4df4d9779a3b68bdbe6
Sheikh Sayed <sksayed@live.com>
2020-07-04 15:22:41 +0600

    flat map needed to study again, initial implementation has been done

--- src/Streams/StreamSimulator.java
+++ src/Streams/StreamSimulator.java
@@ -2,9 +2,14 @@ package Streams;
 
 import sun.font.CreatedFontTracker;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toList;
 
 public class StreamSimulator {
     public static void main(String[] args) {
@@ -14,7 +19,7 @@ public class StreamSimulator {
                 .getDishesList()
                 .stream()
                 .filter(Dishes::isVegetarians)
-                .collect(Collectors.toList());
+                .collect(toList());
         //.forEach(System.out::println);
         vegDishes.stream().forEach(d -> System.out.println(d.getName()));
 
@@ -38,6 +43,31 @@ public class StreamSimulator {
                     return length < 12;
                 })
                 .forEach(System.out::println);
+
+       /* 1.Given a list of numbers,
+        how would you return a list of the square of each number ?
+                For example , given [1,2,3,4,5] you should return [1,4,9,16,25] .*/
+        List<Integer> givenList = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> answereLisr = givenList.stream()
+                .map(eachItem -> eachItem * eachItem)
+                .collect(toList());
+        System.out.println(answereLisr);
+
+       /* 2) Given two lists of numbers ,how would you return all pairsofnumbers ?
+        For example ,given alist[1,2,3] and alist[3,4] you should return[(1,3),(1,4),(2,3),(2,4),(3,3),(3,4)].For
+        simplicity , you can represent a pair as an array with two elements.*/
+
+        List<Integer> firstList = Arrays.asList(1, 2, 3);
+        List<Integer> secondList = Arrays.asList(5, 4);
+
+        List<int[]> pair = firstList.stream()
+                .flatMap( i -> secondList.stream().map( j -> new int[]{i,j}))
+                .collect(Collectors.toList());
+
+        pair.stream().forEach(intArray ->{
+            System.out.println(intArray[0]+" "+intArray[1]);
+        });
 
 
     }
